80bd32dbfd1019c1a2951416d98e5ba877c66a5d
Zaphon Wray <zaphwray@gmail.com>
2025-06-19 07:09:19 -0400

    making updates to test speech

--- main.py
+++ main.py
@@ -14,19 +14,19 @@ async def listen():
             "action": "talk",
             "text": "This is Zaphon and im here to make your life better"
         },
-        {
-            "action": "input",
-            "eventUrl": ["http://127.0.0.1:8000/input"],
-            "type": ["dtmf", "speech"],
-            "dtmf": {
-                "maxDigits": 1,
-                "timeout": 10,
-                "terminator": "#"
-            },
-            "speech": {
-                "context": "You are a helpful assistant that can answer questions and help with tasks."
-            }
-        }
+        # {
+        #     "action": "input",
+        #     "eventUrl": ["http://127.0.0.1:8000/input"],
+        #     "type": ["dtmf", "speech"],
+        #     "dtmf": {
+        #         "maxDigits": 1,
+        #         "timeout": 10,
+        #         "terminator": "#"
+        #     },
+        #     "speech": {
+        #         "context": "You are a helpful assistant that can answer questions and help with tasks."
+        #     }
+        # }
     ]
 
 @app.post("/input")
